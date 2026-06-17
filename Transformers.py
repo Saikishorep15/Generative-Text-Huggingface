fe9a026a45f88149746e7469621c60dc5be03d3e
SaiKishore P <saikishor143p@gmail.com>
2026-06-17 23:27:03 +0530

    Update Transformers.py

--- Transformers.py
+++ Transformers.py
@@ -3,8 +3,10 @@ import torch
 
 torch_device = "cuda" if torch.cuda.is_available() else "cpu"
 
-tokenizer = AutoTokenizer.from_pretrained("gpt2")
+tokenizer = 
+AutoTokenizer.from_pretrained("gpt2")
 
 
 # add the EOS token as PAD token to avoid warnings
-model = AutoModelForCausalLM.from_pretrained("gpt2", pad_token_id=tokenizer.eos_token_id).to(torch_device)
+model = AutoModelForCausalLM.from_pretrained("gpt2", 
+                                             pad_token_id=tokenizer.eos_token_id).to(torch_device)
